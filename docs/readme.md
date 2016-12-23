b5d4acb5db252f93bd6827bc81308cd45deec363
#云淡然 <cloudcome@163.com>
2016-12-23 10:20:13 +0800

    + 继续实现 for 指令

--- docs/readme.md
+++ docs/readme.md
@@ -10,10 +10,12 @@
 - [剖析Vue原理&实现双向绑定MVVM]([link1])
 - [利用 JavaScript 数据绑定实现一个简单的 MVVM 库]([link2])
 - [参照Vue自己来实现MVVM]([link3])
+- [使用莱文斯坦距离（Levenshtein distance）计算两数组的差异]([link4])
 
 
 
 [link1]: http://www.zhijianblog.cn/%E5%89%96%E6%9E%90vue%E5%8E%9F%E7%90%86%E5%AE%9E%E7%8E%B0%E5%8F%8C%E5%90%91%E7%BB%91%E5%AE%9Amvvm/
 [link2]: https://segmentfault.com/a/1190000004847657
 [link3]: https://segmentfault.com/a/1190000007741904
+[link4]: https://leeluolee.github.io/2014/10/21/how-ls-help-you-diff-two-array/
 
--- src/directives/for.js
+++ src/directives/for.js
@@ -9,10 +9,20 @@
 
 var modification = require('blear.core.modification');
 var array = require('blear.utils.array');
+var random = require('blear.utils.random');
 
 var directive = require('./directive');
 var compile = require('../utils/compile');
 var address = require('../utils/address');
+var arrayDiff = require('../utils/array-diff');
+
+var ARRAY_POP = 'pop';
+var ARRAY_PUSH = 'push';
+var ARRAY_REVERSE = 'reverse';
+var ARRAY_SHIFT = 'shift';
+var ARRAY_SORT = 'sort';
+var ARRAY_UNSHIFT = 'unshift';
+var ARRAY_SPLICE = 'splice';
 
 var buildChildMVVM = function (directive, index, data, operation) {
     // 以 parentScope 创建一个实例，这个实例属性是空的，但原型指向 parentScope
@@ -29,13 +39,12 @@ var buildChildMVVM = function (directive, index, data, operation) {
     var operator = operation.operator;
 
     switch (operator) {
-        case 'push':
-        default:
+        case ARRAY_PUSH:
             insertTarget = endAddress;
             insertPosition = 0;
             break;
 
-        case 'unshift':
+        case ARRAY_UNSHIFT:
             insertTarget = startAddress;
             insertPosition = 3;
             break;
@@ -47,14 +56,28 @@ var buildChildMVVM = function (directive, index, data, operation) {
     var watcher = compile(childNode, directive.mvvm, childScope);
 
     switch (operator) {
-        case 'push':
-        case 'unshift':
-        default:
+        case ARRAY_PUSH:
+        case ARRAY_UNSHIFT:
             childScopeList[operator](childScope);
             childNodeList[operator](childNode);
             watcherList[operator](watcher);
             break;
     }
+};
+
+var sortChildScopeListOrder = function (directive) {
+    array.each(directive.childScopeList, function (index, scope) {
+        scope[directive.indexName] = index;
+    });
+};
+
+var moveList = function (list, diff, callback) {
+    var nodes = childNodeList.slice(diff.from, diff.from + diff.howMany);
+    var targetNode = childNodeList[diff.to];
+
+    array.each(nodes, function (index, node) {
+        modification.insert(node, targetNode, 0);
+    }, true);
 };
 
 module.exports = directive({
@@ -90,35 +113,79 @@ module.exports = directive({
         if (the.bound) {
             var operateIndex = operation.operateIndex;
             var operateValue;
-            debugger;
+
             switch (operation.operator) {
-                case 'push':
+                case ARRAY_PUSH:
                     while (operateValue = newVal.shift()) {
                         operateIndex++;
                         buildChildMVVM(the, operateIndex, operateValue, operation);
                     }
                     break;
 
-                case 'pop':
+                case ARRAY_POP:
                     modification.remove(childNodeList.pop());
                     childScopeList.pop();
                     watcherList.pop().destroy();
                     break;
 
-                case 'unshift':
+                case ARRAY_UNSHIFT:
                     operateIndex = newVal.length;
-                    array.each(childScopeList, function (index, scope) {
-                        scope[indexName] = operateIndex + index;
-                    });
                     while (operateValue = newVal.pop()) {
                         operateIndex--;
                         buildChildMVVM(the, operateIndex, operateValue, operation);
                     }
+                    sortChildScopeListOrder(the);
+                    break;
+
+                case ARRAY_SHIFT:
+                    modification.remove(childNodeList.shift());
+                    childScopeList.shift();
+                    watcherList.shift().destroy();
+                    sortChildScopeListOrder(the);
+                    break;
+
+                case ARRAY_SORT:
+                    console.log(oldVal);
+                    console.log(newVal);
+                    var diffs = arrayDiff(oldVal, newVal);
+                    console.log(diffs);
+
+                    var fromOffset = 0;
+                    var toOffset = 0;
+                    array.each(diffs, function (index, diff) {
+                        switch (diff.type) {
+                            // case 'insert':
+                            //     array.each(diff.values, function (index, value) {
+                            //         var spliceIndex = diff.start + index;
+                            //         buildChildMVVM(the, spliceIndex, value, {
+                            //             operator: 'splice',
+                            //             operateIndex: spliceIndex
+                            //         });
+                            //     });
+                            //     break;
+
+                            case 'move':
+                                debugger;
+                                var nodes = childNodeList.slice(diff.from, diff.from + diff.howMany);
+                                var targetNode = childNodeList[diff.to];
+
+                                array.each(nodes, function (index, node) {
+                                    modification.insert(node, targetNode, 0);
+                                }, true);
+                                break;
+
+                            // case 'remove':
+                            //     break;
+                        }
+                    });
+                    sortChildScopeListOrder(the);
                     break;
             }
         } else {
             array.each(data, function (index, data) {
-                buildChildMVVM(the, index, data, {});
+                buildChildMVVM(the, index, data, {
+                    operator: ARRAY_PUSH
+                });
             });
         }
     }
